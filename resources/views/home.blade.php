bbe29397a3198e26193e4ad2db8f3236387e6649
Lazar Jelic <lazarjelic98@gmail.com>
2019-05-05 15:18:30 +0200

    Updated README

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,6 @@
 	</head>
 	<body>
 		<h1>PetPlus API v{{$version}}</h1>                                                                                
-		<p>Developed by <a href="http://www.ecloga.org">Ecloga Apps</a> &copy; {{$year}}</p>
+		<p>Developed by <a href="https://lazarjelic.com/ecloga">Ecloga Apps</a> &copy; {{$year}}</p>
 	</body>
 </html>
